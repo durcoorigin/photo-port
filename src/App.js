74f4c4480cd1b5471e88ec70f31d4a85368e3529
Chris Short <61039651+durcoorigin@users.noreply.github.com>
2020-07-30 20:52:10 -0500

    create SPW functionality

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 
 function App() {
+
+  const[contactSelected, setContactSelected] = useState(false);
+
   const [categories] = useState([
     {
       name: 'commercial', description: 'Photos of grocery stores, food trucks, and other commercial projects',
@@ -22,12 +25,19 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
         <div>
-          <ContactForm></ContactForm>
-          <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+            <ContactForm></ContactForm>
+          )}
         </div>
       </main>
     </div>
